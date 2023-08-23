b83d9577783d35c9ff6e8bb7475341f54759c0d4
Sarmad Gardezi <mail@sarmadgardezi.com>
2023-08-23 20:40:29 +0500

    removed

--- src/Data/Links.js
+++
@@ -1,8 +0,0 @@
-export const links = [
-    {
-        facebookLink: 'https://www.facebook.com/gfx.partner',
-        twitterLink: 'https://twitter.com/gfxpartner',
-        pinterestLink: 'https://www.pinterest.com/gfxpartner',
-        behanceLink: 'https://www.behance.net/gfxpartner',
-    },
-];
--- src/Data/MenuItems.js
+++
@@ -1,7 +0,0 @@
-export const menuItems = [
-    { to: '/', label: 'Home' },
-    { to: '/About', label: 'Pages' },
-    { to: '/Services1', label: 'Services' },
-    { to: '/Project1', label: 'Projects' },
-    { to: '/Bloggrid', label: 'Blog' },
-];
